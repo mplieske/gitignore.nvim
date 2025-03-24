f9c42c98b5aa4d2d6e3b70abdb5f8f69fbd635ac
Martin Plieske <martin.plieske@gmail.com>
2025-03-25 00:43:30 +0100

    Add augroup.

--- lua/gitignore/init.lua
+++ lua/gitignore/init.lua
@@ -1,2 +1,12 @@
-print("Hello from gitignore.nvim!")
+local augroup = vim.api.nvim_create_augroup("GitIgnore", { clear = true })
+
+local function main()
+    print("Hello from gitignore.nvim!")
+end
+
+local function setup()
+    vim.api.nvim_create_autocmd("VimEnter", { group = augroup, desc = "Test 123", once = true, callback = main })
+end
+
+return { setup = setup }
 
