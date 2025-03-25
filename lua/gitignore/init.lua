d9c79f6872c99587b252c03e05e047369e134d70
Martin Plieske <martin.plieske@gmail.com>
2025-03-25 01:55:43 +0100

    Try fix curl data to buffer.

--- lua/gitignore/init.lua
+++ lua/gitignore/init.lua
@@ -25,21 +25,19 @@ local function main()
 
     vim.fn.jobstart('curl https://www.toptal.com/developers/gitignore/dropdown/templates.json', {
         on_stdout = function (jobid, data, event)
-            vim.api.nvim_buf_set_lines(buffer, 0, -1, true, { data })
+            vim.api.nvim_buf_set_lines(buffer, 0, -1, true, data)
+            local window_number = vim.api.nvim_open_win(buffer, true, {
+                relative="win",
+                width=width,
+                height=height,
+                border="single",
+                row=row,
+                col=column,
+                zindex=10001
+            })
+            vim.api.nvim_set_option_value("number", true, { win = window_number })
         end
     })
-
-    local window_number = vim.api.nvim_open_win(buffer, true, {
-        relative="win",
-        width=width,
-        height=height,
-        border="single",
-        row=row,
-        col=column,
-        zindex=10001
-    })
-
-    vim.api.nvim_set_option_value("number", true, { win = window_number })
 end
 
 local function setup()
